8b2490338232e6f7c85890824185dce27b7d5b98
Doo Hwan Cho <doohwancho526@gmail.com>
2019-03-16 14:34:44 +0900

    java

---
+++ java/ToLowerCase.java
@@ -0,0 +1,34 @@
+/*
+ * 
+Implement function ToLowerCase() that has a string parameter str, 
+and returns the same string in lowercase.
+
+Example 1:
+Input: "Hello"
+Output: "hello"
+
+Example 2:
+Input: "here"
+Output: "here"
+
+Example 3:
+Input: "LOVELY"
+Output: "lovely"
+
+ */
+package String;
+
+import java.io.*;
+
+public class ToLowerCase {
+	public static void main(String[] args) {
+		//String letter = "Hello";
+		//String letter = "here";
+		String letter = "LOVELY";
+		
+		String Str = new String(letter);
+		System.out.println(Str.toLowerCase());
+		
+	}
+
+}
