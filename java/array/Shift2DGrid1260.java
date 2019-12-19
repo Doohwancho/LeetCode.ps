1a579766e5ed6f32c467284c514ddc2bf70b2044
Doo Hwan Cho <doohwancho526@gmail.com>
2019-12-20 00:54:40 +0900

    array

--- java/array/Shift2DGrid1260.java
+++ java/array/Shift2DGrid1260.java
@@ -1,10 +1,11 @@
 package Array;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Shift2DGrid1260 {
 	
+	/*
 	//<Trial01>
 	
 	//뭔생각으로 짰는지 원
@@ -49,10 +50,11 @@ public class Shift2DGrid1260 {
         }
         return rst;
     }
-	
+	*/
+	/*
 	//<Trial02>
 	
-	//그리드를 먼저 바꾸고 다 바꾸면 어레이에 옮겨야 하는데 그냥 첫방에 어레이에 옮겨서 불상사남.
+	//한번 돌리는건 되는데, k번(여러번)돌리는건 안됨. 정신차리자 친구야.
 	
 	public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         List<List<Integer>> rst = new ArrayList<>();
@@ -78,4 +80,95 @@ public class Shift2DGrid1260 {
         }
         return rst;
     }
+    */
+	
+	/*
+	//<Trial03>
+	
+	//아니 형 이런 인풋은 좀 아니잖아
+	
+	//Input : [[1],[2],[3],[4],[7],[6],[5]] , 23
+	//Output : [[-1],[-1],[-1],[-1],[-1],[-1],[-1]]
+	//Expected : [[6],[5],[1],[2],[3],[4],[7]]
+	
+	public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> rst = new ArrayList<>();
+        // if(grid.length < 2 && grid[0].length < 2) return rst;
+        
+        int row = grid.length-1;
+        int column = grid[0].length-1;
+        
+        for(int m = 0; m < k; m++){
+            int index = 0;
+            int push1 = grid[index++][0];
+            
+            for(int i = 0; i <= row; i++){
+                //맨 앞에넣을 뒤에꺼 미리 빼두고 
+                int frontKeeper = grid[i][0];
+                int backKeeper = i > 0 ? push1 : grid[row][column];
+                int push2;
+                
+                //오른쪽으로 한칸씩 밀고
+                for(int j = 1; j < column+1; j++){
+                    if(j == 1){
+                        push1 = grid[i][j];
+                        grid[i][j] = grid[i][j-1];        
+                    } else {
+                        push2 = grid[i][j];
+                        grid[i][j] = push1;
+                        push1 = push2;
+                    }
+                }
+                //맨 앞에 삽입
+                grid[i][0] = backKeeper;
+            }
+        }
+        for(int[] p : grid){
+            List<Integer> list = new ArrayList<>();
+                for(int q : p){
+                    list.add(q);
+                }
+                rst.add(list);
+            }
+        return rst;    
+	}
+	*/
+ 
+	//<문제풀이 by JatinYadav>
+	
+	//아.. 이리 쉬운걸
+	
+	//Runtime: 4 ms, faster than 99.13% of Java online submissions for Shift 2D Grid.
+	//Memory Usage: 47 MB, less than 100.00% of Java online submissions for Shift 2D Grid.
+	
+	public static List<List<Integer>> shiftGrid(int[][] grid, int k) {
+	    int row = grid.length;
+	    int column = grid[0].length;  
+	    int mod = row * column;
+	    int[][] temp = new int[row][column];
+	    k = k % mod; //2x2에서 4번 오른쪽으로 움직이면 제자리에 오니까, 불필요한 k제거
+	      
+	    for (int i = 0; i < row; i++) {
+	        for (int j = 0; j < column; j++) {
+	            int p = j + k; // column
+	            int r = p / column; // row
+	            if (p < column) {
+	                temp[i][p] = grid[i][j];
+	            } else {
+	                temp[(i + r) % row][p % column] = grid[i][j];
+	            }
+	        }
+	    }
+	    
+		// temp to list
+	    List<List<Integer>> result = new LinkedList<>();
+	    for (int[] t : temp) {
+	        LinkedList<Integer> c = new LinkedList<>();
+	        for (int i : t) {
+	            c.add(i);
+	        }
+	        result.add(c);
+	    }
+	    return result;
+	}
 }
