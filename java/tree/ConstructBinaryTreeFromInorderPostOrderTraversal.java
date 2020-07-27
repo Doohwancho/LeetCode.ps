2dd8faa70d514679077e1035623f33523e218433
doohwancho <doohwancho526@gmail.com>
2020-07-27 17:20:44 +0900

    july added

--- java/tree/ConstructBinaryTreeFromInorderPostOrderTraversal.java
+++ java/tree/ConstructBinaryTreeFromInorderPostOrderTraversal.java
@@ -17,8 +17,6 @@ public class ConstructBinaryTreeFromInorderPostOrderTraversal {
 	 
 	 
 	//<문제풀이1 by jinwu>
-	
-	//어씨 모르겠네;;
 	  
 	//inorder = [9,3,15,20,7]
 	//postorder = [9,15,7,20,3]
@@ -28,14 +26,20 @@ public class ConstructBinaryTreeFromInorderPostOrderTraversal {
 	//  9  20
 	//    /  \
 	//   15   7
-	  
-	// post order을 뒤집에서 pre order로 바꿈
 	
-	//1) root.right방향으로 postorder에있는 숫자를 역순으로 넣음. inEnd > inStart이 될때까지. 
-	//   inEnd > inStart은 inStart는 inorder.length-1이고, inEnd는 매 iterate마다 for문에서 inorder[i] == postorder[postStart]할때마다 계속커짐.
-	//   근데 왜?
+	//1) root.right방향으로 postorder에있는 숫자를 역순으로 넣음. 왜냐면 postorder의 역순이 pre-order이자너.
 	  
-	//2) 다시 root로 올라오면서 left child를 마저 채워줌.
+	//   언제까지 넣냐면 inEnd > inStart이 될때까지. 
+	//   왜냐면 inorder이 이 문제에서 쓰이는 목적이, inorder는 돌때 순서가 left-root-right순으로 돌기 때문에, inorder에서 마지막을 inStart가 넘어가 버리면,
+	//   더이상 오른쪽에 넣을 애가 없다는 말이니까, 이때부터 왼쪽 child를 채우면 됨.  
+	  
+	//2) 다시 root로 올라오면서 left child를 마저 채워줌. 채울때 
+	//   root.left = buildTree(inorder, idx-1, inEnd, postorder, postStart-1-(inStart - idx)); 을 쓰자너?
+	//   여기서 inEnd는 inorder.length()고, inStart가 inorder.length()-1이었는데 얘를 점점 낮추면서 범위를 늘려감.
+	//   낮추는건 idx-1로. 왜냐면 for문돌려서   if(inorder[i] == postorder[postStart]) idx = i
+	//   하면 idx는 inorder에서 root가 되고, 왼쪽에 더할 child는 inorder에서 idx기준 왼쪽에 있어야 되니까(inorder는 left-root-right순)
+
+	//   그리고 postStart-1-(inStart - idx)이건.... 모르겠네 슈바
 	
 	//Runtime: 0 ms
 	//Memory Usage: 39.5 MB
