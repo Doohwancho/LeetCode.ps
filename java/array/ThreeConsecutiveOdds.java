4bf6acbd0173afe154cc53bf43147bf745144a31
doohwancho <doohwancho526@gmail.com>
2020-08-27 04:15:52 +0900

    array easy question

---
+++ java/array/ThreeConsecutiveOdds.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ThreeConsecutiveOdds {
+	
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int cnt = 0;
+        for(int a : arr){
+            if(a%2 == 1){
+                cnt++;
+                if(cnt == 3) return true;
+            } else{
+                cnt = 0;
+            }
+        }
+        return false;
+    }
+}
