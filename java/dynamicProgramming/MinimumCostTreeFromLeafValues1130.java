b8a566030bf0444380198330841fbdd9c94b33d2
doohwancho <doohwancho526@gmail.com>
2020-12-01 21:30:42 +0900

    dp #5

--- java/dynamicProgramming/MinimumCostTreeFromLeafValues1130.java
+++ java/dynamicProgramming/MinimumCostTreeFromLeafValues1130.java
@@ -1,5 +1,8 @@
 package dynamicProgramming;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MinimumCostTreeFromLeafValues1130 {
 
 	//<Trial01>
@@ -29,6 +32,41 @@ public class MinimumCostTreeFromLeafValues1130 {
                 j--;
                 arr[j] = Math.max(arr[j], arr[j+1]);
             }
+        }
+        
+        return rst;
+    }
+    
+    
+    
+    //<문제풀이1>
+    
+    
+    //Runtime: 3 ms, faster than 45.43% of Java online submissions for Minimum Cost Tree From Leaf Values.
+    //Memory Usage: 36.6 MB, less than 56.10% of Java online submissions for Minimum Cost Tree From Leaf Values.
+    
+    public int mctFromLeafValues(int[] arr) {
+        int rst = 0;
+        int len = arr.length;
+        
+        List<Integer> list = new LinkedList<>();
+        for(int ar : arr) {
+        	list.add(ar);
+        }
+        
+        while(list.size() > 1) {
+        	int idx = 0;
+        	int max = Integer.MAX_VALUE;
+        	for(int i = 0; i < list.size()-1; i++) {
+        		int mult = list.get(i) * list.get(i+1);
+        		if(max > mult) {
+        			max = mult;
+        			idx = i;
+        		}
+        	}
+        	list.set(idx, Math.max(list.get(idx), list.get(idx+1)));
+        	list.remove(idx+1);
+        	rst += max;
         }
         
         return rst;
